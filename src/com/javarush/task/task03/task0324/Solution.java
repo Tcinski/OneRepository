16d708cc688e7ca22f53fe3f195da02dae8e9f62
Tcinski <tcin95@gmail.com>
2020-10-15 21:43:39 +0700

    это сообщение для меня будущего который будет разбираться с этим.

---
+++ src/com/javarush/task/task03/task0324/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task03.task0324;
+
+/* 
+Меркантильные намерения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+         String s = "Я хочу большую зарплату, и для этого изучаю Java";
+         int i = 0;
+         while (i<=10) {
+            System.out.println(s);
+        }
+        }
+    }
+
